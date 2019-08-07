0af8482d90853a7fb6bfe4886cafa6ce0e108c5c
Bruno Reis <bruno.reis@jussi.local>
2019-08-07 17:40:12 -0300

    Adicionando funcao que faz requisicao na api com axios

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(() => {
+    $('#searchForm').on('submit', e => {
+        let searchText = $('#searchText').val();
+        getCountries(searchText);
+        //getCountriesByCapital(searchText);
+        e.preventDefault();
+    });
+});
+
+function getCountries(searchText) {
+    axios.get('https://restcountries.eu/rest/v2/name/'+searchText)
+        .then(res => {
+            //console.log(res.data);
+            let countries = res.data;
+            let output = '';
+            $.each(countries, (index, country) => {
+                output += `
+                <div class="col-md-3">
+                    <div class="well text-center">
+                        <img src="${country.flag}>
+                        <h5>${country.name}</h5>
+                        <a onclick="countrySelected('${country.callingCodes}')" class="btn btn-primary" href="#">Country Details</a>
+                    </div>
+                </div>
+                `
+            });
+            $('#countries').html(output);
+            
+        })
+        .catch(err => {
+            console.log(err);
+        })
+};
