2fb5d02bd9bb9166ede55d828dd5587408bc9e62
Bruno Reis <bruno.reis@jussi.local>
2019-08-07 18:01:43 -0300

    Adicionando CSS

--- js/main.js
+++ js/main.js
@@ -2,32 +2,55 @@ $(document).ready(() => {
     $('#searchForm').on('submit', e => {
         let searchText = $('#searchText').val();
         getCountries(searchText);
-        //getCountriesByCapital(searchText);
+        getCountryByCapital(searchText);
         e.preventDefault();
     });
 });
 
 function getCountries(searchText) {
-    axios.get('https://restcountries.eu/rest/v2/name/'+searchText)
+    axios.get('https://restcountries.eu/rest/v2/name/' + searchText)
         .then(res => {
-            //console.log(res.data);
+            //console.log(res.data)
             let countries = res.data;
             let output = '';
             $.each(countries, (index, country) => {
                 output += `
                 <div class="col-md-3">
                     <div class="well text-center">
-                        <img src="${country.flag}>
+                        <img src="${country.flag}">
                         <h5>${country.name}</h5>
-                        <a onclick="countrySelected('${country.callingCodes}')" class="btn btn-primary" href="#">Country Details</a>
+                        <a onclick="movieSelected('${country.callingCodes}')" class="btn btn-primary" href="#">Country Details</a>
                     </div>
                 </div>
                 `
             });
             $('#countries').html(output);
-            
         })
         .catch(err => {
-            console.log(err);
+            console.log(err)
+        })
+};
+
+function getCountryByCapital(searchText) {
+    axios.get('https://restcountries.eu/rest/v2/capital/' + searchText)
+        .then(res => {
+            //console.log(res.data)
+            let countries = res.data;
+            let output = "";
+            $.each(countries, (index, country) => {
+                output += `
+                <div class="col-md-3">
+                    <div class="well text-center">
+                        <img src="${country.flag}">
+                        <h5>${country.name}</h5>
+                        <a onclick="movieSelected('${country.callingCodes}')" class="btn btn-primary" href="#">Country Details</a>
+                    </div>
+                </div>
+                `
+            });
+            $('#countries').html(output);
+        })
+        .catch(err => {
+            console.log(err)
         })
 };
